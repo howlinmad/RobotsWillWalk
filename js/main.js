bc50eb68f8a7e1123786d92a8aad791a859ff5ad
howlinmad <don.jess.petersen@gmail.com>
2014-10-29 15:13:43 -0700

    animated circle. Next: make bots

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
 // Find the DOM element with the class name "button1"
 var button = document.getElementsByClassName("button1");
 var instructionsDiv = document.getElementsByClassName("instructions");
+var buttonWidth = 25;
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var circleRadians = (2*Math.PI);
+var circleX = 50;
+var circleY = 50;
+var canvasW = canvas.width;
+var canvasH = canvas.height;
 
 // getElementsByClassName returns an array: [<button1>]
 button = button[0];
 instructionsDiv = instructionsDiv[0];
+
 var clickResult = function () {
-  // instructionsDiv has the properties:
-  // instructionsDiv.class = "instructions"
-  // instructionsDiv.style = "border: solid orange 2px; font-family: fantasy"
+  //debugger;
+  button.style.width = (++buttonWidth) + "%"; //increment and typecast a string.
   instructionsDiv.style.border = "solid red 2px";
   console.log("test1");
   //this outputs "test1"
 
 }; // end clickResult()
+
+var drawCircle = function() {
+    context.clearRect(0,0,canvasW,canvasH)
+    context.beginPath();
+    context.arc(circleX, circleY, 50, 0, circleRadians);
+    context.stroke();
+    if(circleX - 50 > canvas.width) 
+        circleX = 25; 
+    else
+        circleX+=1;    
+} //end of drawCircle()
+
+var animateCircle = function() {    
+          
+    setInterval(drawCircle, 30);
+}
